7bf4441f0939c62b2dea3554adf46f7a66d23bfa
Saiqul Haq <saiqulhaq@gmail.com>
2021-12-05 03:37:25 +0700

    fix ruby 3 test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,10 @@ require "bootsnap"
 
 enable_coverage = ENV.fetch("COVERAGE", 0).to_s == '1'
 
+if RUBY_VERSION == 3 && Faraday::VERSION.to_f < 1.2
+  exit(false)
+end
+
 Bootsnap.setup(
   cache_dir:            "tmp/cache",          # Path to your cache
   development_mode:     true,                 # Current working environment, e.g. RACK_ENV, RAILS_ENV, etc
