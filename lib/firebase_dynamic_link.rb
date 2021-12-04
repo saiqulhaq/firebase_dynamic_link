ee291dc79a98ab1951777a8c6736d26ac96f2a37
Saiqul Haq <saiqulhaq@gmail.com>
2021-12-05 03:26:16 +0700

    fix: deprecated method for Dry Configurable 0.13

--- lib/firebase_dynamic_link.rb
+++ lib/firebase_dynamic_link.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-require "dry-configurable"
-require "faraday"
-if Faraday::VERSION.to_f >= 2.0
-  require "faraday/net_http"
-end
-require "firebase_dynamic_link/client"
-require "firebase_dynamic_link/link_renderer"
-require "firebase_dynamic_link/version"
+require 'dry-configurable'
+require 'dry/configurable/version'
+require 'faraday'
+require 'faraday/net_http' if Faraday::VERSION.to_f >= 2.0
+require 'firebase_dynamic_link/client'
+require 'firebase_dynamic_link/link_renderer'
+require 'firebase_dynamic_link/version'
 
 module FirebaseDynamicLink
   extend Dry::Configurable
 
   USE_FARADAY_2 = Faraday::VERSION.to_i == 2
+  USE_DRY_CONFIGURABLE_0_13 = Dry::Configurable::VERSION.to_f == 0.13
 
   # called when invalid configuration given
   class InvalidConfig < StandardError; end
@@ -22,6 +22,10 @@ module FirebaseDynamicLink
 
   # called when Firebase says that no more quota
   class QuotaExceeded < StandardError; end
+
+  if FirebaseDynamicLink::USE_FARADAY_2 && Faraday.default_adapter == :test
+    Faraday.default_adapter = :net_http # default adapter included in this gem
+  end
 
   # @!group Configuration
   # @!method adapter
@@ -37,10 +41,13 @@ module FirebaseDynamicLink
   #     FirebaseDynamicLink.adapter = :net_http_persistent
   #   @see https://github.com/lostisland/faraday/tree/master/test/adapters
   # @since 0.1.0
-  if FirebaseDynamicLink::USE_FARADAY_2 && Faraday.default_adapter == :test
-    Faraday.default_adapter = :net_http # default adapter included in this gem
+  if USE_DRY_CONFIGURABLE_0_13
+    setting :adapter, default: Faraday.default_adapter
+
+  else
+    setting :adapter, Faraday.default_adapter
+
   end
-  setting :adapter, Faraday.default_adapter 
 
   # @!method api_key
   #   @!scope class
@@ -52,25 +59,33 @@ module FirebaseDynamicLink
   # @since 0.1.0
   setting :api_key
 
-  # @!method timout
+  # @!method timeout
   #   Timeout default setting is 3 seconds
   #   @!scope class
-  # @!method timout=(seconds)
+  # @!method timeout=(seconds)
   #   @!scope class
   #   @param seconds [Integer]
   # @since 0.1.0
   # @!scope class
-  setting :timeout, 3
+  if USE_DRY_CONFIGURABLE_0_13
+    setting :timeout, default: 3
+  else
+    setting :timeout, 3
+  end
 
-  # @!method open_timout
+  # @!method open_timeout
   #   @!scope class
   #   Open timeout default setting is 3 seconds
-  # @!method open_timout=(seconds)
+  # @!method open_timeout=(seconds)
   #   @!scope class
   #   @param seconds [Integer]
   # @since 0.1.0
   # @!scope class
-  setting :open_timeout, 3
+  if USE_DRY_CONFIGURABLE_0_13
+    setting :open_timeout, default: 3
+  else
+    setting :open_timeout, 3
+  end
 
   # @!method dynamic_link_domain
   #   @!scope class
@@ -91,8 +106,27 @@ module FirebaseDynamicLink
   #   @param suffix [String]
   # @since 1.0.0
   # @!scope class
-  setting(:suffix_option, "UNGUESSABLE") do |value|
-    %w[SHORT UNGUESSABLE].include?(value) ? value : raise(FirebaseDynamicLink::InvalidConfig, "default suffix option config is not valid")
+  if USE_DRY_CONFIGURABLE_0_13
+    setting(:suffix_option, default: 'UNGUESSABLE', constructor: lambda { |value|
+      if %w[SHORT
+            UNGUESSABLE].include?(value)
+        value
+      else
+        raise(FirebaseDynamicLink::InvalidConfig,
+              'default suffix option config is not valid')
+      end
+    })
+  else
+    setting(:suffix_option, 'UNGUESSABLE') do |value|
+      if %w[SHORT
+            UNGUESSABLE].include?(value)
+        value
+      else
+        raise(FirebaseDynamicLink::InvalidConfig,
+              'default suffix option config is not valid')
+      end
+    end
+
   end
   # @!endgroup
 end
